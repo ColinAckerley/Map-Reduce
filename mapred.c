84242962d0fa7f929f2b598c4a54401231c8b053
Gregory Cook <gc509@rutgers.edu>
2018-10-06 18:22:51 -0400

    wordcnt & sort working for map treads

--- mapred.c
+++ mapred.c
@@ -21,6 +21,7 @@ int largest = 0;
 int smallest = INT_MAX;
 
 struct HashNode **HashTable;
+pthread_mutex_t lock1;
 
 int main(int argc, char **argv){
 	//READ AND PARSE ARGUMENTS
@@ -45,7 +46,7 @@ int main(int argc, char **argv){
 		}
 	}
 	readInput();
-	//FOR DEBUGGING
+	/**FOR DEBUGGING
 	printf("App: %d, Impl: %d, Maps: %d, Reduces: %d, inputCount %d\n", app, impl, maps, reduces,inputCount);
 	fflush(stdout);
 	struct inputList *inNode = inputHead;
@@ -56,30 +57,35 @@ int main(int argc, char **argv){
 		inNode = inNode->next;
 	}
 	printf("\n");
-	
+	**/
+	pthread_mutex_init(&lock1, NULL);
 	HashTable = (struct HashNode **)calloc(1,sizeof(struct HashNode*)*reduces);
 	mapSetup();
 	
-	struct HashNode *temp;
-	for(x=0; x<reduces; x++){
-		temp = HashTable[x];
-		while(temp != NULL){
-			printf("%d ", temp->num);
-			temp = temp->next;
+	/**MORE DEBUGGING
+	if(getpid() == parentID){
+		struct HashNode *temp;
+		for(x=0; x<reduces; x++){
+			temp = HashTable[x];
+			while(temp != NULL){
+				if(app == 1){
+					printf("%d ", temp->num);
+				} else {
+					printf("%s ", temp->string);
+				}
+				temp = temp->next;
+			}
 		}
-	}
-	printf("\n");
-	
+		printf("\n");
+		
+	}**/
 	freeData();
+	pthread_mutex_destroy(&lock1);
 	return 0;
 }
 
 void freeData(){
 	free(HashTable);
-}
-int numCmpFunc (const void * a, const void * b) //Function to compare numbers for quicksort
-{
-   return ( *(int*)a - *(int*)b );
 }
 
 void mapSetup(){
@@ -87,13 +93,16 @@ void mapSetup(){
 	pthread_t *threadIDs;
 	if(impl == 1){
 		threadIDs = (pthread_t *)malloc(sizeof(pthread_t)*maps);
-	} else {
-		parentID = getpid();
 	}
+	parentID = getpid();
 	struct inputList *listChunkEnd, *tempHead, *newHead;
 	newHead = inputHead;
 	//Split input into ceiling of inputCount/maps, last thread will get <= input of the others
-	for(x=0; x<maps; x++){		
+	for(x=0; x<maps; x++){
+		if(nodeCount >= inputCount){
+			pthread_create(&threadIDs[x], NULL, map, NULL);
+			continue;
+		}	
 		nodeCount++;
 		listChunkEnd = newHead;
 		for(y=0; y<(inputCount+maps-1)/maps-1; y++){
@@ -113,21 +122,17 @@ void mapSetup(){
 			fork();
 			if(getpid() != parentID){
 				//CHILD CASE
-				//printf("CHILD %d EXECUTING\n", x);
-				
+				printf("CHILD %d EXECUTING\n", x);
+				map(newHead);
 				break;
 			}
 		}	
 		newHead = tempHead;
-		if(nodeCount >= inputCount){
-			break;
-		}
-
 	}
 	if(impl == 1){
 		for(x=0; x<maps; x++){
 			pthread_join(threadIDs[x],NULL);
-			//printf("Child thread %d joined\n", (int)threadIDs[x]);
+			printf("Child thread %d joined\n", (int)threadIDs[x]);
 		}
 	} else {
 		while(wait(NULL) > 0);
@@ -166,7 +171,9 @@ void readInput(){
 }
 
 void *map(void *dataList){
-	//printf("Child thread\n");
+	if(dataList == NULL){
+		return NULL;
+	}
 	fflush(stdout);
 	//generic map to hash data to the hashtable for reduce step, hash function varies on app type
 	struct inputList *dataPtr = (struct inputList *)dataList;
@@ -180,8 +187,12 @@ void *map(void *dataList){
 			index = hashFuncSort(atoi(dataPtr->data));
 		} else {
 			index = hashFuncWcount(dataPtr->data);
-		}	
-		hashInsert(index, dataPtr);
+		}
+		if(impl == 1){	
+			hashInsert(index, dataPtr);
+		} else {
+			//SHARED MEMORY INSERT
+		}
 		free(dataPtr);
 		dataPtr = temp;		
 	}
@@ -191,24 +202,34 @@ void *map(void *dataList){
 int hashFuncSort(int value){
 	//hash table of size r contains input value range divided by r 
 	int blockSize = (largest-smallest)/reduces;
-	int x, index = 0;	
-	for(x=smallest; x<=largest; x=x+blockSize){
-		if(value >= x && value < x+blockSize+reduces){
-			return index;
+	int x, y = smallest;	
+	for(x=0; x<reduces-1; x++){
+		if(value >= y && value < y+blockSize){
+			return x;
 		} else {
-			index++;
+			y = y+blockSize;
 		}
 	}
-	return index;
+	return x;
 }
 
-int hashFuncWcount(char *value){
-	int index = 0;
-	return index;
+int hashFuncWcount(char *string){
+	int value = (int)tolower(string[0]);
+	int blockSize = (26)/reduces;
+	int x, y = 97;
+	for(x=0; x<reduces-1; x++){
+		if(value < y+blockSize){
+			return x;
+		} else {
+			y = y+blockSize;
+		}
+	}
+	return x;
 }
 
 void hashInsert(int index, struct inputList * dataPtr){
 	//inputs value into hashtable
+	pthread_mutex_lock(&lock1);
 	struct HashNode *currentNode = HashTable[index];
 	struct HashNode *newNode = (struct HashNode *)malloc(sizeof(struct HashNode));
 	if(app == 1){
@@ -226,25 +247,5 @@ void hashInsert(int index, struct inputList * dataPtr){
 		}
 		currentNode -> next = newNode;
 	}
+	pthread_mutex_unlock(&lock1);
 }
-void reduce(int index) //Reduce function for integers
-{
-	int curSize = 0; //Size of the current linked list
-	int linkedListTraverse = 0; //Array index for each linked list node
-	Node *head = hashtable[index]; //Get the head of the linked list from the hashtable
-	Node *linkedList = head; //Pointer to the head to traverse the linked list
-	while(linkedList != NULL) //Gets the size of the current linked list 
-	{
-		curSize++;
-		linkedList = linkedList->next;
-	}
-	int toSort[curSize]; //Creates an array to be used with quicksort 
-	while(linkedList != NULL)
-	{
-		toSort[linkedListTraverse] = linkedList->data; //Copy the data from the linked list into an array 
-		linkedList = linkedList->next;
-		linkedListTraverse++;
-	}
-	qsort(toSort, curSize, sizeof(int), numCmpFunc); //Sort the current node
-}
-
--- mapred.h
+++ mapred.h
@@ -16,8 +16,3 @@ void hashInsert(int, struct inputList *);
 int hashFuncSort(int);
 int hashFuncWcount(char *);
 void *map(void *);
-<<<<<<< HEAD
-=======
-int numCmpFunc (const void *, const void *);
-void reduce(int);
->>>>>>> 68510465e9b84d24b0979e66b3191babb725efb8
